f905cbe55d33c1afc7473f3ec8378448bd8b6e55
Matti Ahinko <matti@ratassoftware.fi>
2014-01-26 06:22:35 +0200

    jo

---
+++ PackageLoss/BackgrondObject.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PackageLoss
+{
+    public class BackgrondObject
+    {
+        Vector2 Position { get; set; }
+        Texture2D texture { get; set; }
+    }
+}
